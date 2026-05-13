60dd4d57c80779f6a1ce77ddd5e3edf02f1ea081
masin <mmgk>
2026-05-13 20:28:24 +0800

    bcommit

---
+++ 1.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+User_Name = input("What is your name? ")
+print("Hello," + User_Name + "!")
+print("Welcome to Python programming.")
+a = int(input("Enter a number:"))
+b = int(input("Enter another number:"))
+sum = a+b
+print("The sum of", a, "and", b, "is",sum)
