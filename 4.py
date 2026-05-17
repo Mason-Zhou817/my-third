5970dfaad15d14d3bf5fb720ddc013f81495747b
masin <mmgk>
2026-05-17 17:20:30 +0800

    添加OpenWeather天气查询示例

---
+++ 4.py
@@ -0,0 +1,8 @@
+from openweather_client import OpenWeatherClient
+
+client = OpenWeatherClient(units="metric", lang="zh_cn")
+# 查询上海的实时天气数据
+weather = client.current_by_city("Shanghai,CN")
+print(weather.temperature)
+print(weather.description)
+print(weather.to_text())
